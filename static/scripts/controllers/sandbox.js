9d42c3ecec30541f694c78d5de44a34815281742
Miguel Ortega Moreno <miguel.ortega.moreno5@gmail.com>
2015-01-23 11:47:02 +0100

    Añadida la pagina web y las dependencias bower

---
+++ static/scripts/controllers/sandbox.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name polymerGeneratedAppApp.controller:SandboxCtrl
+ * @description
+ * # SandboxCtrl
+ * Controller of the polymerGeneratedAppApp
+ */
+angular.module('PolymerBricks')
+  .controller('SandboxCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
