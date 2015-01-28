60c17da3ceec6d70089cca54f3014ec1023db694
Luis <l.ruizr04@gmail.com>
2015-01-28 12:23:02 +0100

    fixes #2

--- src/script_bbdd.py
+++ src/script_bbdd.py
@@ -4,13 +4,11 @@ f = open('src/prueba.txt', 'r')
 connection = httplib.HTTPConnection('localhost:8080')
 headers = {'Content-type': 'application/x-www-form-urlencoded',
 			'Accept': 'text/plain'}
-url = f.readline()
-while url:
-	params = urllib.urlencode({'url': f.readline(),
+for line in f:
+	params = urllib.urlencode({'url': line,
 							'user': '102386634694700574028'})
 	connection.request("POST", '/subir', params, headers)
 	resp = connection.getresponse()
 	response = resp.read()
 	print response
-	url = f.readline()
 connection.close()
