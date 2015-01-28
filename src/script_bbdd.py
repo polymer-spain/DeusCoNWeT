c8280c3a510af88af548d54ef0bac9f14b1cad38
Luis <l.ruizr04@gmail.com>
2015-01-28 01:18:21 +0100

    Alimentacion con fichero

--- src/script_bbdd.py
+++ src/script_bbdd.py
@@ -1,13 +1,16 @@
 import httplib, urllib
 
-#f = open('prueba.txt', 'r')
+f = open('src/prueba.txt', 'r')
 connection = httplib.HTTPConnection('localhost:8080')
 headers = {'Content-type': 'application/x-www-form-urlencoded',
 			'Accept': 'text/plain'}
-params = urllib.urlencode({'url': 'https://github.com/Mortega5/star-rate',
-		'user': '102386634694700574028'})
-connection.request("POST", '/subir', params, headers)
-resp = connection.getresponse()
-response = resp.read()
-print response
+url = f.readline()
+while url:
+	params = urllib.urlencode({'url': f.readline(),
+							'user': '102386634694700574028'})
+	connection.request("POST", '/subir', params, headers)
+	resp = connection.getresponse()
+	response = resp.read()
+	print response
+	url = f.readline()
 connection.close()
