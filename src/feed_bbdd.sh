2ad37e0ab60b691f5d6c2fed3f60b60fbd62e9f9
ailopera <lopera.martinez.ana.isabel@gmail.com>
2015-02-12 09:46:08 +0100

    Modifico script de pruebas. Corrección de pequeños errores

--- src/feed_bbdd.sh
+++ src/feed_bbdd.sh
@@ -1,5 +1,16 @@
-#!/bin/sh 
-while read linea; do
-curl --data $linea http://localhost:8080/componentes
-echo "Haciendo petición POST a http://localhost:8080/componentes"
-done < $1
+#!/bin/sh
+
+
+if $1 == "componente"
+	then
+		while read linea; do
+		curl --data $linea http://localhost:8080/componentes
+		echo "Haciendo petición POST a http://localhost:8080/componentes"
+		done < $1
+elif $1 == "usuario" 
+	then	
+		while read linea; do
+		curl --data $linea http://localhost:8080/usuarios
+		echo "Haciendo petición POST a http://localhost:8080/usuarios"
+		done < $1
+fi
