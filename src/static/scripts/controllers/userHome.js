cdd02c7fbaa52740c63b4ef209943f57e3f6adc7
Miguel Ortega Moreno <miguel.ortega.moreno5@gmail.com>
2015-03-26 16:53:42 +0100

    añadidos fondos

--- src/static/scripts/controllers/userHome.js
+++ src/static/scripts/controllers/userHome.js
@@ -9,34 +9,30 @@
  */
 angular.module('PolymerBricks')
   .controller('userHomeCtrl', function ($scope) {
-  $scope.menuStatus = false;
 
+  $scope.menuStatus = false;
   $scope.showMenu = function(){
 
     if (!$scope.menuStatus){
-      document.querySelector('#menu-button').icon='arrow-forward';
-      document.querySelector('#menu').className="show";
-      setTimeout(function(){
-        $scope.$apply(function(){
-          $scope.menuStatus = !$scope.menuStatus;
-        });
-      },500);
-    }
-    else {
-      document.querySelector('#menu-button').icon='arrow-back';  
-      document.querySelector('#menu').className="hidden";
+      document.querySelector('#menu-icon').icon='arrow-forward';
       $scope.menuStatus = !$scope.menuStatus;
     }
+    else {
+      document.querySelector('#menu-icon').icon='arrow-back';  
+      $scope.menuStatus = !$scope.menuStatus;
+      $scope.selected = '';
+    }
   };
-
-  $scope.buttonShow = function(event) {
-    console.log(event);
-    /*    document.querySelector('#menu-button').icon='arrow-forward';
-    document.querySelector('#menu').className="show";
-    setTimeout(function(){
-      $scope.$apply(function(){
-        $scope.menuStatus = true;
-      });
-    },500);*/
+  $scope.isSelected = function(event) {
+    return $scope.selected === event; 
+  }
+  $scope.setSelected = function(event) {
+    if ($scope.selected === event)
+      $scope.selected = '';
+    else 
+      $scope.selected = event;
+  };
+  $scope.isMenuHidden = function(event) {
+    return !$scope.menuStatus
   }
 });
