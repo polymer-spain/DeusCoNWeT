b54db943892342aaaf49ef1a15f0a490161d0fc4
Miguel Ortega Moreno <miguel.ortega.moreno5@gmail.com>
2015-04-28 15:28:20 +0200

    Arreglado bug con el path

--- src/static/scripts/controllers/main.js
+++ src/static/scripts/controllers/main.js
@@ -23,33 +23,35 @@ angular.module('picbit').controller('MainCtrl', function ($scope, $location, $ti
 
       $scope.hidePopup();// escondemos el popup y cambiamos la direccion del usuario
       if (e.detail.redSocial === 'twitter') {
-        if ($location.pathname.indexOf("profile") !== 0)
+        if ($location.$path.indexOf("profile") !== 0) {
           return
-          $location.path('/user/'+e.detail.redSocial+'_'+e.detail.userId);
-      }
-      else if (e.detail.redSocial === 'googleplus') { // Comprobamos si es google para buscar el id
+        }
+        $location.path('/user/' + e.detail.redSocial + '_' + e.detail.userId);
+      } else if (e.detail.redSocial === 'googleplus') { // Comprobamos si es google para buscar el id
         var xhr = new XMLHttpRequest();
         var uri = 'https://www.googleapis.com/plus/v1/people/me?access_token=' + e.detail.token;
-        xhr.open("GET",uri,true);
-        xhr.onreadystatechange = function() {
-          if (xhr.readyState == 4 && xhr.status === 200){
-            $scope.$apply(function(){
+        xhr.open("GET", uri, true);
+        xhr.onreadystatechange = function () {
+          if (xhr.readyState === 4 && xhr.status === 200) {
+            $scope.$apply(function () {
               var response = JSON.parse(xhr.responseText);
-              $scope.sendData(e.detail.token,response.id,e.detail.redSocial);
-              if($location.pathname.indexOf("profile")!=0)
-                return
-                $location.path('/user/'+e.detail.redSocial+'_'+response.id);              
+              $scope.sendData(e.detail.token, response.id, e.detail.redSocial);
+              if ($location.$$path.indexOf("profile") !== 0) {
+                return;
+              }
+              $location.path('/user/' + e.detail.redSocial + '_' + response.id);
             });
-          } else if (xhr.readyState == 4) {
-            console.log("[INFO]: Algo fue mal en google");
+          } else if (xhr.readyState === 4) {
+            console.info("Algo fue mal en google");
           }
-        }
+        };
         xhr.send();
       } else {// mandamos los datos si ya los tenemos
-        $scope.sendData(e.detail.token,e.detail.userId,e.detail.redSocial);
-        if($location.pathname.indexOf("profile")!=0)
+        $scope.sendData(e.detail.token, e.detail.userId, e.detail.redSocial);
+        if ($location.$$path.indexOf("profile")!=0){
           return
-          $location.path('/user/'+e.detail.redSocial+'_'+e.detail.userId);
+        }
+        $location.path('/user/'+e.detail.redSocial+'_'+e.detail.userId);
       }
       // cambiamos el botton
       var button = document.querySelector('#nameId');
@@ -64,7 +66,7 @@ angular.module('picbit').controller('MainCtrl', function ($scope, $location, $ti
 
     $scope.sendData = function (token, tokenId, redSocial) {
       var request, uri, params;
-      uri = $scope.domain + '/api/oauth/' + redSocial;    
+      uri = 'http://test-frontend.example-project-13.appspot.com/api/oauth/' + redSocial;    
       params = "token_id=" + tokenId + "&access_token=" + token + "&action=login";
       request = {
         method:"post",
@@ -73,7 +75,7 @@ angular.module('picbit').controller('MainCtrl', function ($scope, $location, $ti
         data: params
       }
       $http(request).error(function (data,status) {
-        console.error('Error:' + status + ': no se enviaron datos al backend'); 
+        console.error('Error ' + status + ': no se enviaron datos al backend'); 
       });
 
     };
@@ -93,21 +95,22 @@ angular.module('picbit').controller('MainCtrl', function ($scope, $location, $ti
     $scope.status1 = true;
   }
   /* Escuhas de los botones*/
-  document.querySelector('body').addEventListener('google-logged',$scope.logged);
-  document.querySelector('body').addEventListener('linkedin-logged',$scope.logged);
-  document.querySelector('body').addEventListener('github-logged',$scope.logged);
-  document.querySelector('body').addEventListener('instagram-logged',$scope.logged);
-  document.querySelector('body').addEventListener('twitter-logged',$scope.logged);
-  document.querySelector('body').addEventListener('facebook-logged',$scope.logged);
-  document.querySelector('body').addEventListener('sof-logged',$scope.logged);
+  document.querySelector('body').addEventListener('google-logged', $scope.logged);
+  document.querySelector('body').addEventListener('linkedin-logged', $scope.logged);
+  document.querySelector('body').addEventListener('github-logged', $scope.logged);
+  document.querySelector('body').addEventListener('instagram-logged', $scope.logged);
+  document.querySelector('body').addEventListener('twitter-logged', $scope.logged);
+  document.querySelector('body').addEventListener('facebook-logged', $scope.logged);
+  document.querySelector('body').addEventListener('sof-logged', $scope.logged);
 
   $scope.popup = false;
 
-  $scope.showPopup = function(){
+  $scope.showPopup = function(e){
     if (!$scope.status) {
       $scope.popup = true;
       $scope.shadow = true;
     } else {
+      console.info(e);
       $scope.logout();
     }
   };
