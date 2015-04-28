33d683b20d60b9e07e0d94487f8325f2f45ffe46
ailopera <lopera.martinez.ana.isabel@gmail.com>
2015-04-28 12:37:46 +0200

    Pasa las pruebas (sin tener en cuenta las peticiones que requieren cookies) en los manejadores de linkedin, facebook, instagram, stackoverflow y googleplus (test de #52 y #48)

--- src/test/api_oauth_tester.py
+++ src/test/api_oauth_tester.py
@@ -18,7 +18,7 @@ def make_request(method, request_uri, params, status_ok, session):
   	response = connection.getresponse()
   	responseData = response.read()
   	if not response.status == status_ok:
-  		print ">>> STATUS: ERROR!! " + str(response.status)
+  		print "!!! STATUS: ERROR " + str(response.status)
   		print responseData
   	else:
   		print ">>> STATUS: OK"
@@ -28,7 +28,7 @@ def make_request(method, request_uri, params, status_ok, session):
 
 def main():
 	red_social=sys.argv[1]
-	if red_social == "googleplus" or redsocial=="facebook":
+	if red_social == "googleplus" or red_social=="facebook":
 		basePath = "/api/oauth/" + red_social
 		session = None
 		#Logins
@@ -71,8 +71,8 @@ def main():
 
 		# TEST 6
 		# Get (Sin cookie)
-		request_uri = basePath + "?action=login"
-		print "\nTEST 6: Haciendo petición GET a " + request_uri + " (obtener credenciales)\n Status esperado: 400"
+		request_uri = basePath
+		print "\nTEST 6: Haciendo petición GET a " + request_uri + " (obtener credenciales sin cookie)\n Status esperado: 400"
 		params = urllib.urlencode({})
 		make_request("GET", request_uri, params,400, None)
 
@@ -81,16 +81,19 @@ def main():
 		request_uri = basePath + "?action=login"
 		print "\nTEST 7: Haciendo petición POST a " + request_uri + " (prueba de nueva sesión y actualizar credenciales)\n Status esperado: 200"
 		token_id = "id" + red_social
-		access_token = red_social + "TEST"
+		access_token = red_social + "Modify2TEST"
 		params = urllib.urlencode({'token_id': token_id, 'access_token':access_token})	
-		make_request("POST", request_uri, params,200, None)	
+		make_request("POST", request_uri, params,200, None)
 
-	elif red_social=="stackoverflow" or red_social=="instagram" or red_social=="linkedin"
-	basePath = "/api/oauth/" + red_social
+		# TODO TEST 8
+		# Obtener credenciales con cookie	
+		print "TESTs finalizados. Comprobar las entidades de tipo Usuario y Token almacenadas en datastore"
+
+	elif red_social=="stackoverflow" or red_social=="instagram" or red_social=="linkedin":
+		request_uri = "/api/oauth/" + red_social
 		session = None
-		#Logins
 		# TEST 1
-		request_uri = basePath + "?action=login"
+		# Añadir credenciales nuevas al sistema
 		print "\nTEST 1: Haciendo petición POST a " + request_uri + " (añadir nuevo par de credenciales)\n Status esperado: 201"
 		token_id = "id" + red_social
 		access_token = red_social + "TEST"
@@ -98,30 +101,33 @@ def main():
 		session = make_request("POST", request_uri, params, 201, None)
 
 		# TEST 2
-		request_uri = basePath + "?action=login"
-		print "\nTEST 2: Haciendo petición POST a " + request_uri + " (login de sesion iniciada anteriormente)\n Status esperado: 200"
+		print "\nTEST 2: Haciendo petición POST a " + request_uri + " (actualizar credenciales)\n Status esperado: 200"
 		token_id = "id" + red_social
 		access_token = red_social + "ModifyTEST"
 		params = urllib.urlencode({'token_id': token_id, 'access_token':access_token})
 		make_request("POST", request_uri, params,200, None)	
 
 		# TEST 3
-		request_uri = basePath + "?action=login"
-		print "\nTEST 3: Haciendo petición POST a " + request_uri + " (login)\n Status esperado: 201"
-		token_id = "id" + red_social + "2"
-		access_token = red_social + "TEST"
-		params = urllib.urlencode({'token_id': token_id, 'access_token': access_token})
-		make_request("POST", request_uri, params,201, None)
+		print "\nTEST 3: Haciendo petición POST a " + request_uri + " (actualizar credenciales proporcionando un solo parametro)"
+		print "Status esperado: 400"
+		token_id = "idERROR" + red_social + "2"
+		params = urllib.urlencode({'token_id': token_id})
+		make_request("POST", request_uri, params,400, None)
 
-		# TEST 6
+		# TEST 4
 		# Get (Sin cookie)
-		request_uri = basePath + "?action=login"
-		print "\nTEST 6: Haciendo petición GET a " + request_uri + " (obtener credenciales)\n Status esperado: 400"
+		print "\nTEST 4: Haciendo petición GET a " + request_uri + " (obtener credenciales sin cookie)\n Status esperado: 400"
 		params = urllib.urlencode({})
 		make_request("GET", request_uri, params,400, None)
 
+		# TEST 4
+		# TODO Get (Con Cookie)
 
+		print "TESTs finalizados. Comprobar las entidades de tipo Usuario y Token almacenadas en datastore"
 
+	else:
+		print "Error: es obligatorio proporcionar un parámetro válido para indicar que red social se pretende testear"
+		print "Uso: python api_oauth_tester.py {googleplus|stackoverflow|facebook|instagram|linkedin|github|twitter}"
 	# Cerramos conexión
 	connection.close()
 
