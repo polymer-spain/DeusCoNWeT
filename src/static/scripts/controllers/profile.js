f8276ae5d564dbb03e1a8a23a1bd723939066ef9
Miguel Ortega Moreno <miguel.ortega.moreno5@gmail.com>
2015-05-04 16:56:35 +0200

    Añadidos los parametros de las redes sociales

--- src/static/scripts/controllers/profile.js
+++ src/static/scripts/controllers/profile.js
@@ -1,16 +1,16 @@
-'user strict'
-angular.module('picbit')
-  .controller('ProfileCtrl', function ($scope) {
+angular.module('picbit').controller('ProfileCtrl', function ($scope) {
+  'use strict';
 
-  $scope.calC= function()	{	 
-    var nombre=document.getElementById("nombre").value;
-    var apellido=document.getElementById("apellido").value;
-    var mail=document.getElementById("email").value;	
-    var dia=document.getElementById("day").value;
-    var month=document.getElementById("month").value;
-    var year=document.getElementById("year").value;
-    var descr=document.getElementById("descr").value;	
-    var sex=document.getElementById("sexo").value;	
+  $scope.calC = function () {
+    var nombre, apellido, mail, dia, month, year, descr, sex;
+    nombre = document.getElementById("nombre").value;
+    apellido = document.getElementById("apellido").value;
+    mail = document.getElementById("email").value;
+    dia = document.getElementById("day").value;
+    month = document.getElementById("month").value;
+    year = document.getElementById("year").value;
+    descr = document.getElementById("descr").value;
+    sex = document.getElementById("sexo").value;
     console.log(nombre);
     console.log(apellido);
     console.log(mail);
@@ -20,69 +20,55 @@ angular.module('picbit')
     console.log(descr);
 
 
-    if(sex==1){
-      console.log("Mujer");	
-      sex="Mujer";	
+    if (sex === 1) {
+      console.log("Mujer");
+      sex = "Mujer";
     }
 
-    if(sex==2){
-      console.log("Hombre");	
-      sex="Hombre";
+    if (sex === 2) {
+      console.log("Hombre");
+      sex = "Hombre";
     }
-    if(sex==3){
-      console.log("Otros");	
-      sex="Otros";
-    }
-
-
   };
 
-  $scope.takeInfoLog = function(e){
+  $scope.takeInfoLog = function (e) {
 
     if (e.detail.redSocial === 'googleplus') {
-      var googleplus="googleplus";
+      var googleplus = "googleplus";
       console.log("googleplus");
       document.getElementById("google").status = '';
 
     }
     if (e.detail.redSocial === 'facebook') {
-      var facebook="facebook";
+      var facebook = "facebook";
       console.log("facebook");
 
     }
     if (e.detail.redSocial === 'github') {
-      var github="github";
+      var github = "github";
       console.log("github");
-
     }
     if (e.detail.redSocial === 'linkedin') {
-      var linkedin="linkedin";
+      var linkedin = "linkedin";
       console.log("linkedin");
 
     }
     if (e.detail.redSocial === 'instagram') {
-      var instagram="instagram"
+      var instagram = "instagram"
       console.log("instagram");
 
     }
     if (e.detail.redSocial === 'twitter') {
-      var twitter="twitter";
+      var twitter = "twitter";
       console.log("twitter");
-
     }
 
   };
 
-
-  document.getElementById("pr").addEventListener('google-logged',$scope.takeInfoLog);
-  document.getElementById("pr").addEventListener('facebook-logged',$scope.takeInfoLog);
-  document.getElementById("pr").addEventListener('linkedin-logged',$scope.takeInfoLog);
-  document.getElementById("pr").addEventListener('github-logged',$scope.takeInfoLog);
-  document.getElementById("pr").addEventListener('instagram-logged',$scope.takeInfoLog);
-  document.getElementById("pr").addEventListener('twitter-logged',$scope.takeInfoLog);
-
-
-
-
-})
-
+  document.getElementById("pr").addEventListener('google-logged', $scope.takeInfoLog);
+  document.getElementById("pr").addEventListener('facebook-logged', $scope.takeInfoLog);
+  document.getElementById("pr").addEventListener('linkedin-logged', $scope.takeInfoLog);
+  document.getElementById("pr").addEventListener('github-logged', $scope.takeInfoLog);
+  document.getElementById("pr").addEventListener('instagram-logged', $scope.takeInfoLog);
+  document.getElementById("pr").addEventListener('twitter-logged', $scope.takeInfoLog);
+});
