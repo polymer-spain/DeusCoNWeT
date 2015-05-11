b498475f8094f601813edcff57f70e067c08b2db
ailopera <lopera.martinez.ana.isabel@gmail.com>
2015-05-11 10:51:52 +0200

    Avances en la implementación de GET de usuario

--- src/api_handlers/api_usuarios.py
+++ src/api_handlers/api_usuarios.py
@@ -21,12 +21,12 @@
 import webapp2
 from google.appengine.ext import ndb
 from google.appengine.api import memcache
-
-class UserListHandler(webapp2.RequestHandler):
+from api_oauth import SessionHandler
+class UserListHandler(SessionHandler):
 
     """
   Class that defines the user resource
-  It acts as the handler of the /usuarios/{user_id} resource
+  It acts as the handler of the /usuarios/ resource
   Methods:
   get -- Returns a list of all the users stored in the system
   post -- Adds a new user to the system
@@ -46,42 +46,41 @@ class UserListHandler(webapp2.RequestHandler):
         self.response.write(json.dumps(results))
 
   # POST Method
+    # def post(self):
+    #     """ Adds a new user to the system
+    # Keyword arguments: 
+    #   self -- info about the request build by webapp2
+    # """
 
-    def post(self):
-        """ Adds a new user to the system
-    Keyword arguments: 
-      self -- info about the request build by webapp2
-    """
+    #     name = self.request.get('name', default_value='None')
+    #     email = self.request.get('email', default_value='None')
+    #     if not name == 'None' and not email == 'None':
 
-        name = self.request.get('name', default_value='None')
-        email = self.request.get('email', default_value='None')
-        if not name == 'None' and not email == 'None':
+    #   # Checks if the user was previously stored
 
-      # Checks if the user was previously stored
+    #         user = Usuario.query(Usuario.email == email).get()
+    #         if user == None:
 
-            user = Usuario.query(Usuario.email == email).get()
-            if user == None:
+    #     # Creates the new user
 
-        # Creates the new user
+    #             newUser = Usuario(nombre=name, identificador=name,
+    #                               email=email, lista_Redes=[],
+    #                               lista_Grupos=[])
+    #             newUser.put()
+    #             self.response.set_status(200)
+    #         else:
 
-                newUser = Usuario(nombre=name, identificador=name,
-                                  email=email, lista_Redes=[],
-                                  lista_Grupos=[])
-                newUser.put()
-                self.response.set_status(200)
-            else:
+    #     # Returns a Not Modified status
 
-        # Returns a Not Modified status
+    #             self.response.set_status(304)
+    #     else:
 
-                self.response.set_status(304)
-        else:
+    #   # Returns a Bad Request status
 
-      # Returns a Bad Request status
-
-            self.response.set_status(400)
+    #         self.response.set_status(400)
 
 
-class UserHandler(webapp2.RequestHandler):
+class UserHandler(SessionHandler):
 
     """
   Class that defines the user resource
@@ -98,22 +97,16 @@ class UserHandler(webapp2.RequestHandler):
       self -- info about the request build by webapp2
       user_id -- id of the user 
     """
-
-    # Returns the component queried
-
-        user = Usuario.query(Usuario.identificador == user_id).get()
-        if user == None:
-            self.response.set_status(404)
+      cookie_value = self.request.cookies.get('session')
+      if not cookie_value == None:
+        user = self.getuserInfo(cookie_value)
+        if not user == None and user_id == user:
+          # TODO: Returns the public and private info related to an user
         else:
-
-      # Builds the response
-
-            response = {
-                'name': user.nombre,
-                'user_id': user.identificador,
-                'email': user.email,
-                'network_list': user.lista_Redes,
-                'group_list': user.lista_Grupos,
-                }
-            self.response.content_type = 'application/json'
-            self.response.write(json.dumps(response))
+          response = {'error': 'The cookie session provided does not belong to any active user'}
+          self.response.content_type = 'application/json'
+          self.response.write(json.dumps(response))
+          self.response.set_status(401)
+      else:
+        # TODO: Returns the public info about the user
+        buscaUsuario()
