a033caa03320560c8ea80bd13ab3060fac6586bf
lruizr <l.ruizr04@gmail.com>
2015-06-15 15:47:48 +0200

    Lista de usuarios

--- src/api_handlers/api_usuarios.py
+++ src/api_handlers/api_usuarios.py
@@ -5,17 +5,17 @@
   Copyright 2015 Miguel Ortega Moreno
   Copyright 2015 Juan Francisco Salamanca Carmona
 
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
 
-       http://www.apache.org/licenses/LICENSE-2.0
+      http://www.apache.org/licenses/LICENSE-2.0
 
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
 """
 
 import webapp2
@@ -24,7 +24,7 @@ from google.appengine.api import memcache
 from api_oauth import SessionHandler
 class UserListHandler(SessionHandler):
 
-    """
+  """
   Class that defines the user resource
   It acts as the handler of the /usuarios/ resource
   Methods:
@@ -35,13 +35,19 @@ class UserListHandler(SessionHandler):
   # GET Method
 
     def get(self):
-      cookie = self.request.get('session')
-      if not cookie == None:
-        user = self.getUserInfo(cookie) # Entity_key of user
+      users_list = ndb_pb.getUsers()
+      if len(users_list) == 0:
+        self.response.content_type = 'application/json'
+        self.response.write('')
+        self.response.set_status(204)
+      else:
+        self.response.content_type = 'application/json'
+        self.response.write(users_list)
+        self.response.set_status(200)
 
 class UserHandler(SessionHandler):
 
-    """
+  """
   Class that defines the user resource
   It acts as the handler of the /usuarios/{user_id} resource
   Methods:
