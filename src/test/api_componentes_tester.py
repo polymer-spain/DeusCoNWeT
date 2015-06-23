98816902033e00c4f10892b379c802f7755fc33e
ailopera <lopera.martinez.ana.isabel@gmail.com>
2015-06-23 12:35:48 +0200

    Pequeñas anotaciones en el fichero de pruebas de componentes

--- src/test/api_componentes_tester.py
+++ src/test/api_componentes_tester.py
@@ -9,6 +9,7 @@
 # TEST 1: Obtener la lista de componentes. Debe retornar una lista vacia de componentes
 # Status esperado: 204 
 
+# TESTs PUT lista de componentes (Subir un componente al sistema) 
 # TEST 2: Subir un componente al sistema, proporcionando una uri incorrecta.
 # Status esperado: 404
 
@@ -22,11 +23,15 @@
 # TEST 5: Subir un componente al sistema (componente 2).
 # Status esperado: 200
 
+# TESTs Metodo GET Lista de componentes 
 # TEST 6: Obtener la lista de componentes, sin proporcionar una cookie de sesion
 # Status esperado: 401
 
 # TEST 7 : Obtener la lista de componentes, proporcionando una cookie de sesion
 # Status esperado: 200
 
+# TODO TESTs Metodo GET Componente (obtener info de un componente en particular)
+# TODO TESTs Metodo POST Componente (modificar info de un componente)
+# TODO TESTs Metodo DELETE Componente 
 
 # POST-TEST 1: Hacer logout en el sistema mediante googleplus
