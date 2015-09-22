8c50e026395620d484f9c1568026869e066d731a
ailopera <lopera.martinez.ana.isabel@gmail.com>
2015-09-22 11:56:33 +0200

    Borro todo para hacer una PR desde develop segura

--- src/test/feed_usuarios.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Script para añadir usuarios a la datastore en local
-# Uso: ./feed_usuarios.sh prueba_usuarios.txt
-
-while read linea; do
-curl --data $linea http://localhost:8080/usuarios
-echo "Haciendo petición POST a http://localhost:8080/usuarios"
-done < $1
