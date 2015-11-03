c67c4599fa15dc3151bb465c623f330fbefb0292
ailopera <lopera.martinez.ana.isabel@gmail.com>
2015-11-03 11:40:45 +0100

    Pequeñas modificaciones en la numeración de las pruebas

--- src/test/backend/api_componentes_usuarios_tester.py
+++ src/test/backend/api_componentes_usuarios_tester.py
@@ -92,44 +92,44 @@ def main():
 			params = urllib.urlencode({})
 			test_utils.make_request("GET", request_uri, params, 204, session1)
 
-			# TEST 4B
-			print "TEST 4B: Obtener la lista de componentes del usuario2, proporcionando una cookie de sesion"
+			# TEST 5
+			print "TEST 5: Obtener la lista de componentes del usuario2, proporcionando una cookie de sesion"
 			print "(parámetro de filtrado por usuario)"
 			print "Status esperado: 200"
 			request_uri = components_basepath + "?filter=user"
 			params = urllib.urlencode({})
 			test_utils.make_request("GET", request_uri, params, 200, session2)
 
-			# TEST 5
-			print "TEST 5: Obtener la lista de componentes del usuario 2, proporcionando una cookie de sesion"
+			# TEST 6
+			print "TEST 6: Obtener la lista de componentes del usuario 2, proporcionando una cookie de sesion"
 			print "(Combinamos el parámetro de filtrado por red social y el filtrado por usuario con el formato de lista reducido)"
 			print "Status esperado: 204 (El usuario no tiene componentes de la red social Twitter)"
 			request_uri = components_basepath + "?social_network=twitter&filter=user&list_format=reduced"
 			test_utils.make_request("GET", request_uri, params, 204, session1)
 
-			# TEST 6
-			print "TEST 6: Obtener la lista de componentes del usuario 2, proporcionando una cookie de sesion"
+			# TEST 7
+			print "TEST 7: Obtener la lista de componentes del usuario 2, proporcionando una cookie de sesion"
 			print "(Combinamos el parámetro de filtrado por red social y el filtrado por usuario con el formato de lista reducido)"
 			print "En este caso obtenemos los componentes de linkedin del usuario"
 			print "Status esperado: 200"
 			request_uri = components_basepath + "?social_network=linkedin&filter=user&list_format=reduced"
 			test_utils.make_request("GET", request_uri, params, 200, session2)
 
-			# TEST 7
-			print "TEST 7: Obtener la lista de componentes, proporcionando una cookie de sesion"
+			# TEST 8
+			print "TEST 8: Obtener la lista de componentes, proporcionando una cookie de sesion"
 			print "(Combinamos el parámetro de filtrado por red social y el filtrado por usuario con el formato de lista completo)"
 			print "Status esperado: 204"
 			request_uri = components_basepath + "?social_network=linkedin&filter=user&list_format=complete"
 			test_utils.make_request("GET", request_uri, params, 204, session1)
 
-			# TEST 8
-			print "TEST 8: Obtener info de usuario"
+			# TEST 9
+			print "TEST 9: Obtener info de usuario 1"
 			print "Status esperado: 200"
 			request_uri = users_basepath + "/" + user_id1
 			test_utils.make_request("GET", request_uri, params, 200, session1)
 
-			# TEST 9
-			print "TEST 9: Obtener info de usuario, con formato de lista de componentes detallado"
+			# TEST 10
+			print "TEST 9: Obtener info de usuario 1, con formato de lista de componentes detallado"
 			print "Status esperado: 200"
 			request_uri = users_basepath + "/" + user_id1 + "?component_info=detailed"
 			test_utils.make_request("GET", request_uri, params, 200, session1)
@@ -137,15 +137,15 @@ def main():
 		elif option == 'dashboard_borrado':
 			component_rel_uri = "/linkedin-timeline"
 
-			# PRETEST 4
-			print "PRETEST 4: Añadimos el componente al dashboard de usuario 2, si no está añadido ya"
+			# PRETEST 5
+			print "PRETEST 5: Añadimos el componente al dashboard de usuario 2, si no está añadido ya"
 			print "Status esperado: 200 (Ignorar status de este caso)"
 			request_uri = users_basepath + "/" + user_id2
 			params = urllib.urlencode({'component': 'linkedin-timeline'})
 			test_utils.make_request("POST", request_uri, params, 200, session2, preTest=True)
 
-			# PRETEST 5
-			print "PRETEST 5: Obtenemos la info de usuario, con objeto de ver los componentes que tiene incluidos en su dashboard"
+			# PRETEST 6
+			print "PRETEST 6: Obtenemos la info de usuario, con objeto de ver los componentes que tiene incluidos en su dashboard"
 			print "Status esperado: 200"
 			request_uri = users_basepath + "/" + user_id2 + "?component_info=detailed"
 			params = urllib.urlencode({})
@@ -153,67 +153,67 @@ def main():
 
 			# TESTs relativos al borrado de componentes de usuario
 			# Pruebas de casos de error
-			# TEST 10
-			print "TEST 10: Borrar el componente del usuario, sin cookie"
+			# TEST 11
+			print "TEST 11: Borrar el componente del usuario, sin cookie"
 			print "Status esperado: 401"
 			request_uri = components_basepath + component_rel_uri
 			test_utils.make_request("DELETE", request_uri, params, 401, None)
 
-			# TEST 11
-			print "TEST 11: Borrar el componente del usuario, proporcionando una cookie incorrecta"
+			# TEST 12
+			print "TEST 12: Borrar el componente del usuario, proporcionando una cookie incorrecta"
 			print "Status esperado: 400"
 			request_uri = components_basepath + component_rel_uri
 			test_utils.make_request("DELETE", request_uri, params, 400, session_error)
 
-			# TEST 12
-			print "TEST 12: Borrar el componente del usuario, a un componente que no existe"
+			# TEST 13
+			print "TEST 13: Borrar el componente del usuario, a un componente que no existe"
 			print "Status esperado: 400"
 			request_uri = components_basepath + "/component_error"
 			test_utils.make_request("DELETE", request_uri, params, 400, session_error)
 
 			# Casos de éxito
-			# TEST 13
-			print "TEST 13: Eliminar componente del usuario 2, con cookie de sesión correcta"
+			# TEST 14
+			print "TEST 14: Eliminar componente del usuario 2, con cookie de sesión correcta"
 			print "Status esperado: 200"
 			request_uri = components_basepath + component_rel_uri
 			test_utils.make_request("DELETE", request_uri, params, 200, session2)
 
-			# TEST 14
-			print "TEST 14: Obtener info de usuario 2"
+			# TEST 15
+			print "TEST 15: Obtener info de usuario 2"
 			print "(No debe aparecer el componente eliminado en la lista de componentes de usuario)"
 			print "Status esperado: 200"
 			request_uri = users_basepath + "/" + user_id2
 			test_utils.make_request("GET", request_uri, params, 200, session2)
 
-			# TEST 15
-			print "TEST 15: Volver a añadir el componente"
+			# TEST 16
+			print "TEST 16: Volver a añadir el componente"
 			print "Status esperado: 200"
 			request_uri = users_basepath + "/" + user_id2
 			params = urllib.urlencode({'component': 'linkedin-timeline'})
 			test_utils.make_request("POST", request_uri, params, 200, session2)
 
-			# TEST 16
-			print "TEST 16: Eliminar el componente del dashboard del usuario 1"
+			# TEST 17
+			print "TEST 17: Eliminar el componente del dashboard del usuario 1"
 			print "Status esperado: 200"
 			params = urllib.urlencode({})
 			request_uri = components_basepath + component_rel_uri + "?scope=user"
 			test_utils.make_request("DELETE", request_uri, params, 200, session2)
 
-			# TEST 17
-			print "TEST 17: Obtener lista filtrada de componentes (filter=user), proporcionando la cookie de sesión del usuario 1"
+			# TEST 18
+			print "TEST 18: Obtener lista filtrada de componentes (filter=user), proporcionando la cookie de sesión del usuario 1"
 			print "(No debe aparecer el componente eliminado)"
 			print "Status esperado: 204"
 			request_uri = components_basepath + "?filter=user&list_format=complete"
 			test_utils.make_request("GET", request_uri, params, 204, session2)
 
-			# TEST 18
-			print "TEST 18: Intentar eliminar el componente del dashboard del usuario 1, estando eliminado ya"
+			# TEST 19
+			print "TEST 19: Intentar eliminar el componente del dashboard del usuario 1, estando eliminado ya"
 			print "Status esperado: 404"
 			request_uri = components_basepath + component_rel_uri + "?scope=user"
 			test_utils.make_request("DELETE", request_uri, params, 404, session2)
 
-			# TEST 19
-			print "TEST 19: Obtener info sobre el componente"
+			# TEST 20
+			print "TEST 20: Obtener info sobre el componente"
 			print "(Para verificar que no se ha eliminado por error el componente general)"
 			print "Status esperado: 200"
 			request_uri = components_basepath + component_rel_uri
@@ -272,9 +272,9 @@ def main():
 			}, doseq=True)
 			test_utils.make_request("PUT", request_uri, params, 201, None, preTest=True)
 
-			# TEST 20: Creamos un nuevo usuario en el sistema (Realizando login mediante facebook)
+			# TEST 21: Creamos un nuevo usuario en el sistema (Realizando login mediante facebook)
 			request_uri = "/api/oauth/facebook/signup"
-			print "TEST 20: Signup de usuario 3 en el sistema\n Ignorar el status de este caso"
+			print "TEST 21: Signup de usuario 3 en el sistema\n Ignorar el status de este caso"
 			print "Probaremos que al añadir un nuevo usuario al sistema, se le añadan los componentes predeterminados"
 			print "Ignorar el status de salida de este TEST"
 			print "Status esperado: 201 "
@@ -285,8 +285,8 @@ def main():
 			session3 = test_utils.make_request("POST", request_uri, params, 201, None, True)
 
 
-			# TEST 21: Creamos un nuevo usuario en el sistema (Realizando login mediante facebook)
-			print "TEST 21: Signup de usuario 4 en el sistema\n Ignorar el status de este caso"
+			# TEST 22: Creamos un nuevo usuario en el sistema (Realizando login mediante facebook)
+			print "TEST 22: Signup de usuario 4 en el sistema\n Ignorar el status de este caso"
 			print "Probaremos que al añadir un nuevo usuario al sistema, se le añadan los componentes predeterminados"
 			print "Ignorar el status de salida de este TEST"
 			print "Status esperado: 201 "
@@ -296,8 +296,8 @@ def main():
 			 'user_identifier': user_id4 })
 			session4 = test_utils.make_request("POST", request_uri, params, 201, None, True)
 
-			# TEST 22: Creamos un nuevo usuario en el sistema (Realizando login mediante googleplus)
-			print "TEST 22: Signup de usuario 5 en el sistema\n Ignorar el status de este caso"
+			# TEST 23: Creamos un nuevo usuario en el sistema (Realizando login mediante googleplus)
+			print "TEST 23: Signup de usuario 5 en el sistema\n Ignorar el status de este caso"
 			print "Probaremos que al añadir un nuevo usuario al sistema, se le añadan los componentes predeterminados"
 			print "Ignorar el status de salida de este TEST"
 			print "Status esperado: 201 "
@@ -307,16 +307,15 @@ def main():
 			 'user_identifier': user_id5 })
 			session5 = test_utils.make_request("POST", request_uri, params, 201, None, True)
 
-
-			# TEST 23
-			print "TEST 23: Intentamos añadir un componente predeterminado al usuario 3"
+			# TEST 24
+			print "TEST 24: Intentamos añadir un componente predeterminado al usuario 3"
 			print "Status esperado: 304 (Ya estaba añadido anteriormente)"
 			request_uri = users_basepath + "/" + user_id3
 			params = urllib.urlencode({'component': 'insignia-fb-prededetermined'})
 			test_utils.make_request("POST", request_uri, params, 304, session3)
 
-			# TEST 24: Añadimos un componente cualquiera al usuario
-			print "TEST 24: Añadimos un componente cualquiera al usuario 3"
+			# TEST 25: Añadimos un componente cualquiera al usuario
+			print "TEST 25: Añadimos un componente cualquiera al usuario 3"
 			print "Status esperado: 200"
 			request_uri = users_basepath + "/" + user_id3
 			params = urllib.urlencode({'component': 'fb-messages'})
@@ -326,20 +325,20 @@ def main():
 			params = urllib.urlencode({})
 			request_uri = components_basepath + "?filter=user&list_format=complete"
 			
-			# TEST 25
-			print "TEST 25: Listamos los detalles sobre los componentes de usuario 3."
+			# TEST 26
+			print "TEST 26: Listamos los detalles sobre los componentes de usuario 3."
 			print "(Deben aparecer los componentes predeterminados y el añadido)"
 			print "Status esperado: 200"
 			test_utils.make_request("GET", request_uri, params, 200, session3)
 
-			# TEST 26
-			print "TEST 26: Listamos los detalles sobre los componentes de usuario 4."
+			# TEST 27
+			print "TEST 27: Listamos los detalles sobre los componentes de usuario 4."
 			print "(Deben aparecer los componentes predeterminados)"
 			print "Status esperado: 200"
 			test_utils.make_request("GET", request_uri, params, 200, session4)
 
-			# TEST 27
-			print "TEST 27: Listamos los detalles sobre los componentes de usuario 5."
+			# TEST 28
+			print "TEST 28: Listamos los detalles sobre los componentes de usuario 5."
 			print "(Deben aparecer los componentes predeterminados)"
 			print "Status esperado: 200"
 			test_utils.make_request("GET", request_uri, params, 200, session5)
