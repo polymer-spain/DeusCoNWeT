304514bef71d4c08242eea19229d99e8f783f7dd
Miguel Ortega Moreno <mortega@conwet.com>
2016-04-27 21:58:26 +0200

    Added language to main structure

--- src/static/scripts/controllers/main.js
+++ src/static/scripts/controllers/main.js
@@ -1,14 +1,19 @@
 /*global angular, document, console*/
-angular.module('picbit').controller('MainController', ['$scope', 'RequestLanguage', function ($scope, RequestLanguage) {
+angular.module('picbit').controller('MainController', ['$scope', 'RequestLanguage', '$location', '$cookies', function ($scope, RequestLanguage, $location, $cookies) {
 
   'use strict';
 
-  $scope.languageRequest = function(file){
+  $scope.domain = 'https://' + $location.host(); // Dominio bajo el que ejecutamos
+
+  // Language control
+  $scope.idioma = $cookies.get('language') || $window.navigator.language;
+  $scope.languageRequest = function(lang){
+    var file = lang + "_"+ lang + ".json";
     RequestLanguage.language(file).success(function (data){
       $scope.language = data;
       $scope.languageSelected = data.lang[$scope.idioma];
     });
   };
-  $scope.languageRequest('es_es.json');
-  
+  $scope.languageRequest($scope.idioma);
+
 }]);
