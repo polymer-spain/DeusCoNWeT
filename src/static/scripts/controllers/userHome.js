92a5b9a06004c94c193479babcb697e6f5b2b12a
Miguel Ortega Moreno <mortega@conwet.com>
2016-06-02 15:45:01 +0200

    added twitter token

--- src/static/scripts/controllers/userHome.js
+++ src/static/scripts/controllers/userHome.js
@@ -9,7 +9,7 @@ angular.module('picbit').controller('UserHomeController', ['$scope', '$timeout',
 	$scope.facebookData = {};
 
 	$scope.instagramData.token = '2062815740.34af286.169a9c42e1404ae58591d066c00cb979';
-	$scope.twitterData.token = 'GITHUB-TOKEN';
+	$scope.twitterData.token = '3072043347-T00ESRJtzlqHnGRNJZxrBP3IDV0S8c1uGIn1vWf';
 	$scope.githubData.username = 'mortega5';
 
 	$scope.listComponents = [
