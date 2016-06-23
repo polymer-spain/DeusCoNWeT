8a398c5bbaab72351b161d7b7b2fc0b022403568
lruizr <l.ruizr04@gmail.com>
2016-06-23 15:48:54 +0200

    Fixed the empty response for components list

--- src/api_handlers/ndb_pb.py
+++ src/api_handlers/ndb_pb.py
@@ -950,6 +950,7 @@ def getComponents(entity_key=None, rs="", all_info=False, filter_by_user=False):
           general_comp["url"] = component.url
           general_comp["social_network"] = component.rs
           general_comp["description"] = component.description
+          general_comp["preversion"] = component.preasigned_version
           if not rate == None: 
             general_comp["rate"] = rate.rating_value
           else:
