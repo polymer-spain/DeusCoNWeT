9b9558f96ddeae00f819800675b6a51fc79c2b3a
lruizr <l.ruizr04@gmail.com>
2016-06-27 15:47:04 +0200

    Added the new path to the profile resource

--- src/manejador_pb.py
+++ src/manejador_pb.py
@@ -37,6 +37,7 @@ app = webapp2.WSGIApplication([
     
     (r'/api/usuarios', api_usuarios.UserListHandler),
     (r'/api/usuarios/(.*)', api_usuarios.UserHandler),
+    (r'/api/usuarios/(.*)/profile', api_usuarios.ProfileHandler),
     
     (r'/api/aux/twitterTimeline', api_auxiliar.OAuthTwitterTimelineHandler),
     (r'/api/aux/instagramTimeline', api_auxiliar.instagramRequest),
