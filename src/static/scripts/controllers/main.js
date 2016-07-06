e2ea86bd3dfc76fa8bc7bc67d50f34d85cafafbb
Miguel Ortega Moreno <mortega@conwet.com>
2016-07-06 16:09:21 +0200

    Added css, js and html linters (css doesnt work correctly). Added backend call when user add new token

--- src/static/scripts/controllers/main.js
+++ src/static/scripts/controllers/main.js
@@ -2,7 +2,7 @@
 angular.module('picbit').controller('MainController', ['$scope', 'RequestLanguage', '$location', '$cookies', '$backend', '$http', '$rootScope', function ($scope, RequestLanguage, $location, $cookies, $backend, $http, $rScope) {
 
 	'use strict';
-	if ($location.host() == "localhost"){
+	if ($location.host() === "localhost"){
 		$scope.domain = "http://" + $location.host() + ":" + $location.port();
 	}else {
 		$scope.domain = 'https://' + $location.host(); // Dominio bajo el que ejecutamos
@@ -27,45 +27,43 @@ angular.module('picbit').controller('MainController', ['$scope', 'RequestLanguag
 			if (parent){
 				document.getElementById(parent).close();
 			}
-			$location.path('/')
-		}, function(response){
+			$location.path('/');
+		}, function(){
 			$('html').css('cursor','');
-			$location.path('/')
+			$location.path('/');
 			//console.error('Error ' + response.status + ': Fallo al intentar realizar un logout del usurio ' + $rScope.user.name);
 		});
 	};
+
 	// Login callback function
 	$scope.loginProcess = function(userData){
-		/* Cogemos el identificador del usuario */
-		function newUser(userData) {
+		$rScope.token = userData.token;
+		// buscamos si existe el usuario
+		$backend.getUserId(userData.userId, userData.redSocial)
+		.then(function (responseUserId) {
+			/* Pedimos la información del usuario y la almacenamos para poder acceder a sus datos */
+			var user = responseUserId.data;
+			// actualizamos los datos del usuario en el servidor
+			$backend.sendData(userData.token, userData.userId, responseUserId.data.user_id, userData.redSocial, userData.oauth_verifier)
+			.then(function() {
+				// Si todo va bien, le mandamos a su dashboard
+				$location.path('/user/' + user.user_id);
+			}, function(responseLogin) {
+				console.error('Error ' + responseLogin.status + ': al intentar mandar los datos de login');
+			});
+		}, function(){// Si response con error, significa que no existe el usuario
 			$rScope.register = {token: userData.token, redSocial: userData.redSocial, tokenId: userData.userId, oauthVerifier: userData.oauth_verifier};
 			$location.path('/selectId');
-		}
-		$rScope.token = userData.token;
-		if ($location.$$path.indexOf('profile') === -1) {
-			$backend.getUserId(userData.userId, userData.redSocial)
-				.then(function (responseUserId) { /* Si devuelve un 200, ya existe el usuario*/
-				/* Pedimos la información del usuario y la almacenamos para poder acceder a sus datos */
-				var user = responseUserId.data;
-				$backend.sendData(userData.token, userData.userId, responseUserId.data.user_id, userData.redSocial, userData.oauth_verifier)
-					.then(function() {
-					$location.path('/user/' + user.user_id);
-				}, function(responseLogin) {
-					console.error('Error ' + responseLogin.status + ': al intentar mandar los datos de login'); 
-				});
-			}, function(){newUser(userData)});
-		} else {
-			$backend.sendData(userData.token, user.user_id, userData.redSocial);
-		}
+		});
 	};
-	$scope.pathname = window.location.pathname;
+	
 	$scope.goto = function(view){
 		if (view.indexOf(':user') > -1){
 			view = view.replace(/:user/g,$rScope.user.user_id);
 		}
 		$location.path(view); // path not hash
 
-	}
+	};
 	var loginCallback = function (e) {
 		//$scope.hidePopup();// escondemos el popup y cambiamos la direccion del usuario
 		$('#login-popup').modal('hide');
@@ -74,25 +72,25 @@ angular.module('picbit').controller('MainController', ['$scope', 'RequestLanguag
 		var uri;
 		switch(socialNetwork) {
 			case 'googleplus':
-				uri = 'https://www.googleapis.com/plus/v1/people/me?access_token=' + e.detail.token;
-				$http.get(uri).success(function (responseData) {
-					e.detail.userId = responseData.id;
-					$scope.loginProcess(e.detail);
-				});
-				break;
-			case 'twitter':
-				uri = $backend.endpoint + '/api/oauth/twitter/authorization/' + e.detail.oauth_verifier;
-				$http.get(uri).success(function (responseData) {
-					e.detail.userId = responseData.token_id;
-					$scope.loginProcess(e.detail);
-				}).error(function() {
-					console.log('Problemas al intentar obtener el token_id de un usuario' );
-				});
-				break;
-			default:
+			uri = 'https://www.googleapis.com/plus/v1/people/me?access_token=' + e.detail.token;
+			$http.get(uri).success(function (responseData) {
+				e.detail.userId = responseData.id;
 				$scope.loginProcess(e.detail);
-				break;
-		};
+			});
+			break;
+			case 'twitter':
+			uri = $backend.endpoint + '/api/oauth/twitter/authorization/' + e.detail.oauth_verifier;
+			$http.get(uri).success(function (responseData) {
+				e.detail.userId = responseData.token_id;
+				$scope.loginProcess(e.detail);
+			}).error(function() {
+				console.log('Problemas al intentar obtener el token_id de un usuario' );
+			});
+			break;
+			default:
+			$scope.loginProcess(e.detail);
+			break;
+		}
 	};
 
 	// Binding login callback
@@ -101,10 +99,10 @@ angular.module('picbit').controller('MainController', ['$scope', 'RequestLanguag
 		$('#login-popup twitter-login')[0].addEventListener('twitter-logged', loginCallback);
 		$('#login-popup login-facebook')[0].addEventListener('facebook-logged', loginCallback);
 	})();
-	
+
 	// avoid language errors
-	$scope.$watch('language.delete', function(newValue, oldValue) {
-  $('.icon-delete > span').html(newValue);
-});
+	$scope.$watch('language.delete', function(newValue) {
+		$('.icon-delete > span').html(newValue);
+	});
 
 }]);// end angular.module
