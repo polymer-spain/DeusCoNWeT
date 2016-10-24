693d9fe4722972d62ac263e68a9c9a2e9fe6fd4d
lruizr <l.ruizr04@gmail.com>
2016-10-24 13:13:14 +0200

    Added a new route to assign components to a user

--- src/manejador_pb.py
+++ src/manejador_pb.py
@@ -38,6 +38,7 @@ app = webapp2.WSGIApplication([
     (r'/api/usuarios', api_usuarios.UserListHandler),
     (r'/api/usuarios/(.*)', api_usuarios.UserHandler),
     (r'/api/usuarios/(.*)/profile', api_usuarios.ProfileHandler),
+    (r'/api/usuarios/(.*)/assign', api_usuarios.AssignComponentsHandler),
     (r'/api/usuarios/(.*)/credentials', api_usuarios.UserCredentialsHandler),
 
     (r'/api/aux/twitterTimeline', api_auxiliar.OAuthTwitterTimelineHandler),
