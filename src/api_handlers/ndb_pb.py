3826193d9c977ffbd81c4c62231bf45ee110a00f
lruizr <l.ruizr04@gmail.com>
2016-11-24 13:35:34 +0100

    Changed the way to insert the pinterest component

--- src/api_handlers/ndb_pb.py
+++ src/api_handlers/ndb_pb.py
@@ -771,6 +771,8 @@ def insertComponent(name, url="", description="", rs="", input_t=None, output_t=
   elif rs == "facebook":
     attributes = ComponentAttributes(component_id=name, access_token="", component_directory=component_directory)
     attributes.put()
+  elif rs == "pinterest":
+    attributes = ComponentAttributes(component_id=name, access_token="", component_base='bower_components/pinterest/')
   initial_index = random.randint(0, len(version_list)-1)
   component = Component(component_id=name, url=url, input_type=input_t, output_type=output_t,
    rs=rs, description=description, version_list=version_list, version_index=initial_index, predetermined=predetermined,
