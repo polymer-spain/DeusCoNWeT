d9dc783dd3867d0f11659d0d147f911186d67894
Miguel Ortega Moreno <mortega@conwet.com>
2017-01-17 13:06:42 +0100

    Fixed twitter dependencies

--- src/static/bower_components/twitter-timeline-stable/README.md
+++ src/static/bower_components/twitter-timeline-stable/README.md
@@ -1,4 +1,4 @@
-# twitter-timeline
+# twitter-timeline [![Build Status](https://travis-ci.org/JuanFryS/twitter-timeline.svg?branch=master)](https://travis-ci.org/JuanFryS/twitter-timeline)
 
 It is a [Polymer](https://www.polymer-project.org/1.0/) component for obtain the timeline of Twitter.
 
--- src/static/bower_components/twitter-timeline-stable/api.py
+++ src/static/bower_components/twitter-timeline-stable/api.py
@@ -11,19 +11,26 @@ import oauth
 
 class OAuthTwitterHandler(webapp2.RequestHandler):
     def get(self):
-        consumer_key = self.request.get("consumer_key", default_value="")
-        consumer_secret = self.request.get("consumer_secret", default_value="")
-        access_token = self.request.get("access_token", default_value="")
-        secret_token = self.request.get("secret_token", default_value="")
-        count = self.request.get("count", default_value="200")
+      
+# Obtención de datos desde fichero en sistema
+#      fich = open("tweets.json")
+#      fich = fich.read()
+#      self.response.write(fich)
 
-        client = oauth.TwitterClient(consumer_key, consumer_secret, "oob")
+#Obtención de datos desde servidor red social
+      consumer_key = self.request.get("consumer_key", default_value="")
+      consumer_secret = self.request.get("consumer_secret", default_value="")
+      access_token = self.request.get("access_token", default_value="")
+      secret_token = self.request.get("secret_token", default_value="")
+      count = self.request.get("count", default_value="200")
 
-        respuesta = client.make_request(
-            "https://api.twitter.com/1.1/statuses/home_timeline.json",
-            token=access_token, secret=secret_token,additional_params={"count": count}, protected=True)
-        
-        self.response.write(respuesta.content)
+      client = oauth.TwitterClient(consumer_key, consumer_secret, "oob")
+
+      respuesta = client.make_request(
+      	"https://api.twitter.com/1.1/statuses/home_timeline.json",
+      	token=access_token, secret=secret_token,additional_params={"count": count}, protected=True)
+
+      self.response.write(respuesta.content)
 
 app = webapp2.WSGIApplication([
         (r'/oauth/twitter', OAuthTwitterHandler)
