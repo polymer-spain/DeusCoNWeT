1239fc198d983b51d694674401980ebcfee84db7
Miguel Ortega Moreno <mortega@conwet.com>
2017-09-11 12:13:05 +0200

    Change dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
 FROM mgood/appengine-python
 
 RUN apt-get update -y && apt-get install -y vim less git stunnel4 net-tools curl
-RUN mkdir /app
+RUN mkdir -p /var/www
 RUN pip install pycrypto mongoengine
-
+COPY . /var/www/
 COPY ./server_config/stunnel4 /etc/default/ 
 COPY ./server_config/stunnel.* /etc/stunnel/
--- server_config/init.sh
+++ server_config/init.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 service stunnel4 start
-dev_appserver.py --port 80 --host 0.0.0.0 --datastore_consistency_policy consistent --storage_path /logs/picbit /app/src 2> /logs/picbit.log
+dev_appserver.py --port 80 --host 0.0.0.0 --datastore_consistency_policy consistent --storage_path /logs/picbit /var/www/src 2> /logs/picbit.log
